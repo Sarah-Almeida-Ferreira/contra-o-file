5c1fd3ee0a5c555034ab6c93f21c90e397d3dad8
Sarah <sarahalmeida013@gmail.com>
2024-11-19 08:09:20 -0300

    Corrigindo para remover warnings do lint

--- src/__tests__/components/Header.test.tsx
+++ src/__tests__/components/Header.test.tsx
@@ -1,5 +1,5 @@
-import React, { act, AnchorHTMLAttributes, ReactNode } from "react";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import React, { AnchorHTMLAttributes, ReactNode } from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Header from "../../components/Header";
 
 // Mock para evitar problemas com `gatsby-plugin-image`
